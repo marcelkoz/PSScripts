866f23223326060ea6f32ba75a296397b3d1dbc7
Marcel Kozlowski <Marcel Kozlowski>
2023-05-21 00:34:13 +0100

    Get-InstalledPrograms.ps1: Add support for querying Microsoft Store apps

--- Get-InstalledPrograms.ps1
+++ Get-InstalledPrograms.ps1
@@ -11,47 +11,100 @@ $RegistryLocation = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall'
 class Program
 {
     [string] $Name
+    [string] $Type
     [string] $Publisher
     [string] $Location
 }
 
-function GetPrograms
+function GetProgramsFromRegistry
 {
     $raw = Get-ChildItem $RegistryLocation
     $objects = ($raw | ForEach-Object { Get-ItemProperty $_.PSPath })
-    return FilterProgramProperties $objects
+    return ConvertRegistryPrograms $objects
 }
 
-function FilterProgramProperties
+function ConvertRegistryPrograms($programs)
 {
-    param (
-        $programs
-    )
-
-    $filtered = New-Object System.Collections.Generic.List[System.Object]
+    $converted = New-Object System.Collections.Generic.List[System.Object]
     foreach ($program in $programs)
     {
         $object = [Program]::new()
-        if ($null -eq $program.DisplayName)
-        {
-            # use key name as program name instead
-            $object.Name = (Split-Path -Leaf $program.PSPath)
-        }
-        else
-        {
-            $object.Name = $program.DisplayName
-        }
-
+        $object.Name = (ParseRegistryProgamName $program)
+        $object.Type = 'Installer'
         $object.Publisher = $program.Publisher
         $object.Location = $program.InstallLocation
 
-        $filtered.Add($object)
+        $converted.Add($object)
     }
-    return $filtered.ToArray()
+    return $converted.ToArray()
 }
 
-function StartScript {
-    $programs = GetPrograms | Sort-Object -Property Name
+function ParseRegistryProgamName($program)
+{
+    if ($null -eq $program.DisplayName)
+    {
+        # use key as program name instead
+        return (Split-Path -Leaf $program.PSPath)
+    }
+    else
+    {
+        return $program.DisplayName
+    }
+}
+
+function GetProgramsFromStore
+{
+    return ConvertStorePrograms (Get-AppxPackage)
+}
+
+function ConvertStorePrograms($programs)
+{
+    $converted = New-Object System.Collections.Generic.List[System.Object]
+    foreach ($program in $programs)
+    {
+        $object = [Program]::new()
+        $object.Name = $program.Name
+        $object.Type = 'Store'
+        $object.Publisher = (ParseStorePublisherName $program.Publisher)
+        $object.Location = $program.InstallLocation
+
+        $converted.Add($object)
+    }
+
+    return $converted.ToArray()
+}
+
+function ParseStorePublisherName($publisher)
+{
+    if ($publisher -eq '' -or $null -eq $publisher)
+    {
+        return ''
+    }
+
+    # CN=subdivison, O=publisher/organisation...
+    $sections = $publisher.Split(',')
+    if ($sections.Length -eq 0)
+    {
+        return $publisher
+    }
+    elseif ($sections.Length -eq 1)
+    {
+        # CN=returned
+        return $sections[0].Split('=')[1]
+    }
+    else
+    {
+        # CN=..., O=returned
+        return $sections[1].Split('=')[1]
+    }
+}
+
+function StartScript
+{
+    Write-Host 'NOTE: Run as administrator to view everything.'
+    $registry = GetProgramsFromRegistry
+    $store = GetProgramsFromStore
+    $programs = $registry + $store | Sort-Object -Property Name
     if ($Path -ne '')
     {
         $programs | ConvertTo-Json | Out-File -FilePath $Path
